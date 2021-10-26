cf3598ad8a62a055596758f781b7be42ea0c2574
shoya <m.syoya0508@gmail.com>
2021-10-26 15:56:11 +0000

    PHP02の課題提出

---
+++ php02kadai.php
@@ -0,0 +1,37 @@
+<?php
+
+$a = 3;
+$b = 7;
+echo $a + $b;
+
+$array_month = ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
+echo $array_month[9];
+
+$hello = "Hello,";
+$name = "shoya";
+$world = "'s World!";
+echo $hello.$name.$world;
+
+$tech_boost = "tech";
+$tech_boost .= " boost";
+echo $tech_boost;
+
+$calender_2018 = [
+    "January" => "1月",
+    "February" => "2月",
+    "March" => "3月",
+    "April" => "4月",
+    "May" => "5月",
+    "June" => "6月",
+    "July" => "7月",
+    "Auguest" => "8月",
+    "September" => "9月",
+    "October" => "10月",
+    "November" => "11月",
+    "December" => "12月"
+];
+echo $calender_2018["December"];
+
+
+
+
